f8e1b45294f3a6dab7e51d7dfa470a6953bfa6c5
owent <admin@owent.net>
2020-09-30 12:53:46 +0800

    优化 tframe utils 导入

--- 3rd_party/atframe_utils/libatframe_utils.cmake
+++ 3rd_party/atframe_utils/libatframe_utils.cmake
@@ -2,9 +2,14 @@
     include_guard(GLOBAL)
 endif()
 
-if (NOT TARGET ${3RD_PARTY_ATFRAME_UTILS_LINK_NAME})
-    add_subdirectory(${3RD_PARTY_ATFRAME_UTILS_PKG_DIR})
-endif ()
+if(TARGET ${3RD_PARTY_ATFRAME_UTILS_LINK_NAME})
+    message(STATUS "${3RD_PARTY_ATFRAME_UTILS_LINK_NAME} already exist, use it for ${PROJECT_NAME} directly.")
+else()
+    if (NOT EXISTS "${CMAKE_BINARY_DIR}/deps/${3RD_PARTY_ATFRAME_UTILS_LINK_NAME}")
+        file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/deps/${3RD_PARTY_ATFRAME_UTILS_LINK_NAME}")
+    endif ()
+    add_subdirectory(${3RD_PARTY_ATFRAME_UTILS_PKG_DIR} "${CMAKE_BINARY_DIR}/deps/${3RD_PARTY_ATFRAME_UTILS_LINK_NAME}")
+endif()
 
 # =========== 3rdparty atframe_utils ==================
 list(APPEND PROJECT_LIBATAPP_PUBLIC_LINK_NAMES ${3RD_PARTY_ATFRAME_UTILS_LINK_NAME})
