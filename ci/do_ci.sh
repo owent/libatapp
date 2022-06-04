f4eca68d50651e652114aef68510d9db05562f55
WenTao Ou <admin@owent.net>
2022-06-04 11:46:37 +0800

    Retry mingw cache

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -95,12 +95,12 @@ elif [[ "$1" == "clang.test" ]]; then
   cmake --build . -j2 --config $CONFIGURATION || cmake --build . --config $CONFIGURATION
   ctest -VV . -C $CONFIGURATION -L libatapp.unit_test
 elif [[ "$1" == "msys2.mingw.test" ]]; then
-  # pacman -S --needed --noconfirm mingw-w64-x86_64-cmake git m4 curl wget tar autoconf automake \
-  #   mingw-w64-x86_64-git-lfs mingw-w64-x86_64-toolchain mingw-w64-x86_64-libtool \
-  #   mingw-w64-x86_64-python mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-setuptools || true
+  pacman -S --needed --noconfirm mingw-w64-x86_64-cmake git m4 curl wget tar autoconf automake \
+    mingw-w64-x86_64-git-lfs mingw-w64-x86_64-toolchain mingw-w64-x86_64-libtool \
+    mingw-w64-x86_64-python mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-setuptools || true
   export ATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_PROTOBUF_ALLOW_LOCAL=1
   # pacman -S --needed --noconfirm mingw-w64-x86_64-protobuf
-  # git config --global http.sslBackend openssl
+  git config --global http.sslBackend openssl
   mkdir -p build_jobs_ci
   cd build_jobs_ci
   cmake .. -G 'MinGW Makefiles' "-DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS" -DPROJECT_ENABLE_UNITTEST=ON -DPROJECT_ENABLE_SAMPLE=ON \
